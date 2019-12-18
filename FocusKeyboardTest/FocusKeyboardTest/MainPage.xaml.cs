0fa3507f25b2ba21f10202333ca50aea1acabc71
Nathan Coit <nathan.coit@fieldpoint.net>
2019-12-18 09:41:15 -0500

    Sample project

---
+++ FocusKeyboardTest/FocusKeyboardTest/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FocusKeyboardTest
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void UserFocusedEntry_Focused(object sender, FocusEventArgs e)
+        {
+            if(UserFocusedEntry.IsFocused)
+            {
+                UserFocusedEntry.Unfocus();
+            }
+            ProgramFocusedEntry.Focus();
+        }
+
+        private void UserFocusedEntryDatePicker_Focused(object sender, FocusEventArgs e)
+        {
+            if (UserFocusedEntryDatePicker.IsFocused)
+            {
+                UserFocusedEntryDatePicker.Unfocus();
+            }
+            ProgramFocusedDatePicker.Focus();
+        }
+    }
+}
